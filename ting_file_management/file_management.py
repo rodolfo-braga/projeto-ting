257587f0a22e8be65dd4d1aef1c4143abf593583
rodolfo-braga <rodolfo.braga@yahoo.com.br>
2022-07-15 11:48:14 -0300

    feat: implements function txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if path_file.endswith('.txt'):
+        try:
+            with open(path_file, 'r') as f:
+                file = f.read()
+                return file.split('\n')
+        except FileNotFoundError:
+            sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
+    else:
+        sys.stderr.write('Formato inválido\n')
