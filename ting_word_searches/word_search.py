f3d427b36685f4c64b8252a855ce3ddff350bbf5
rodolfo-braga <rodolfo.braga@yahoo.com.br>
2022-07-15 14:07:08 -0300

    feat(word_search): implements function search_by_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -20,4 +20,22 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    search = []
+    for i in range(instance.__len__()):
+        file = instance.search(i)
+        occurrences = []
+        file_lines = file['linhas_do_arquivo']
+        number_of_lines = len(file_lines)
+        for line in range(number_of_lines):
+            if word.lower() in file_lines[line].lower():
+                occurrences.append({
+                    'linha': line + 1,
+                    'conteudo': file_lines[line]
+                })
+        if len(occurrences) > 0:
+            search.append({
+                'palavra': word,
+                'arquivo': file['nome_do_arquivo'],
+                'ocorrencias': occurrences
+            })
+    return search
