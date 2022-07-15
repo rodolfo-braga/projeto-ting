61abf6fcca2d5c683f939f6d8aeaba81f3321f69
rodolfo-braga <rodolfo.braga@yahoo.com.br>
2022-07-15 12:08:42 -0300

    feat(file_process): implements function file_metadata

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,3 +1,4 @@
+import sys
 from ting_file_management.file_management import txt_importer
 
 
@@ -24,4 +25,7 @@ def remove(instance):
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        print(instance.search(position))
+    except IndexError:
+        sys.stderr.write("Posição inválida")
