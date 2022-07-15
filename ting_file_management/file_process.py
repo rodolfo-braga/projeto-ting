c206e014a1c8d9713a074109470878258d4e4987
rodolfo-braga <rodolfo.braga@yahoo.com.br>
2022-07-15 11:56:33 -0300

    feat(file_process): implements function process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,18 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for i in range(instance.__len__()):
+        if instance.search(i)['nome_do_arquivo'] == path_file:
+            return
+    imported_file = txt_importer(path_file)
+    processed_file = {
+        'nome_do_arquivo': path_file,
+        'qtd_linhas': len(imported_file),
+        'linhas_do_arquivo': imported_file
+    }
+    instance.enqueue(processed_file)
+    print(processed_file)
 
 
 def remove(instance):
